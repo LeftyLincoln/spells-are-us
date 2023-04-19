7b608f66285ea5eda7ccc0d6869e84f07a06a750
Travis Parks <travisparks0788@gmail.com>
2023-04-19 11:42:45 -0400

    Add styling for background

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,10 @@
 import React from "react"
+import "./Home.css"
 
 export const Home = () => {
   return (
     <div className="home-page">
-      <button>🪄</button>
+      <img src="" alt="wand"/>
     </div>
   )
 }
