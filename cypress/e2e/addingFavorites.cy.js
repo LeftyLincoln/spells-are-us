eb6c548d0daad200c2ee8875c4c872cfb3e22ae2
Travis Parks <travisparks0788@gmail.com>
2023-04-20 13:35:06 -0400

    Add testing for favorite and deleting

--- cypress/e2e/addingFavorites.cy.js
+++ cypress/e2e/addingFavorites.cy.js
@@ -12,4 +12,15 @@ describe('Home Page', () => {
     cy.visit("http://localhost:3000/favoriteSpells")
     cy.get('.no-fav-message').contains("You haven't saved any spells yet...")
   })
+
+  it('should allow the user to save favorite spells and delete spells', () => {
+    cy.get('[href="/allSpells"] > button').click()
+    cy.get(':nth-child(1) > .spell-button').click()
+    cy.get(':nth-child(5) > .spell-button').click()
+    cy.get('[href="/favoriteSpells"] > button').click()
+    cy.get(".favorite-card").should("have.length", 2)
+    cy.get(".favorite-card").last().contains("Anapneo")
+    cy.get(':nth-child(1) > .favorite-button').click()
+    cy.get(".favorite-card").should("have.length", 1)
+  })
 })
