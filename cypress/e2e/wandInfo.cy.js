868d61c1593dd0d0dfb4081faa246d99fa999000
Travis Parks <travisparks0788@gmail.com>
2023-04-20 15:48:32 -0400

    Add testing for wandInfo section

--- cypress/e2e/wandInfo.cy.js
+++ cypress/e2e/wandInfo.cy.js
@@ -1,5 +1,19 @@
 describe('Wand Info', () => {
   beforeEach(() => {
+    cy.intercept(
+      "GET",
+      "https://hp-api.onrender.com/api/spells",
+      { fixture: "spells.json" }
+    )
     cy.visit("http://localhost:3000/wandInfo")
+  })
+
+
+  it("should show information about wands to the user on a unique url", () => {
+    cy.get('.quote').contains("The wand chooses the wizard... it's not always clear why.")
+    cy.get('.facts > :nth-child(3)').contains("Ollivander & Gregorovitch")
+    cy.get('.facts > :nth-child(5)').contains("Unicorn Hair")
+    cy.url().should('eq', "http://localhost:3000/wandInfo")
+    cy.get('.wand-image-info').should('have.attr', 'src').should('include','wandImage')
   })
 })
